af591883f3a7aef988b64c564e67f3ea880ec698
TcWong <1394031559@qq.com>
2020-08-22 21:23:05 +0800

    过期信息显示

--- pengWeb/src/utils/api.js
+++ pengWeb/src/utils/api.js
@@ -8,28 +8,31 @@ axios.interceptors.request.use(config => {
 })
 
 //响应拦截
-axios.interceptors.response.use(data=>{
+axios.interceptors.response.use(data => {
   if (data.status && data.status == 200 && data.data.code == 500) {
     //后台报错
-    Message.error({message:data.data.message});
+    Message.error({message: data.data.message});
     return;
   }
   if (data.data.message) {
     Message.success({message: data.data.message})
   }
   return data.data;
-},err=>{
+}, err => {
   if (err.response.status == 504 || err.response.status == 404) {
-    Message.error({message:'服务器在开小差，请稍等！'})
-  }else if (err.response.status == 403) {
-    Message.error({message: '权限不足，请联系管理员'})
-  }else if (err.response.status == 401) {
+    Message.error({message: '服务器在开小差，请稍等！'})
+  }else if (err.response.status == 413) {
     Message.error({message:err.response.data.message})
-  }else {
+    return err.response.data;
+  } else if (err.response.status == 403) {
+    Message.error({message: '权限不足，请联系管理员'});
+  } else if (err.response.status == 401) {
+    Message.error({message: err.response.data.message});
+  } else {
     if (err.response.data.message) {
-      Message.error({message:err.response.data.message})
-    }else {
-      Message.error({message:"未知错误"})
+      Message.error({message: err.response.data.message})
+    } else {
+      Message.error({message: "未知错误"})
     }
   }
 })
@@ -53,36 +56,36 @@ export const postKeyValueRequest = (url, params) => {
     }
   });
 }
-export const postRequest = (url,params) =>{
+export const postRequest = (url, params) => {
   return axios({
     method: "post",
-    url:`${base}${url}`,
+    url: `${base}${url}`,
     data: params,
     header: {
-      'Content-Type':'application/json'
+      'Content-Type': 'application/json'
     }
   });
 }
-export const putRequest = (url,params) =>{
+export const putRequest = (url, params) => {
   return axios({
-    method:'put',
-    url:`${base}${url}`,
-    data:params,
-    header:{
-      'Content-Type':'application/json'
+    method: 'put',
+    url: `${base}${url}`,
+    data: params,
+    header: {
+      'Content-Type': 'application/json'
     }
   });
 }
-export const deleteRequest = (url)=>{
+export const deleteRequest = (url) => {
   return axios({
-    method:'delete',
-    url:`${base}${url}`
+    method: 'delete',
+    url: `${base}${url}`
   })
 }
-export const getRequest = (url) =>{
+export const getRequest = (url) => {
   return axios({
-    method:'get',
-    url:`${base}${url}`
+    method: 'get',
+    url: `${base}${url}`
   })
 }
 
