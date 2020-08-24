f84385ba2bfcc94ad8c2cdc2e83ebed500b89cef
TcWong <1394031559@qq.com>
2020-08-24 19:10:11 +0800

    修改路由加载方式

--- pengWeb/src/router/index.js
+++ pengWeb/src/router/index.js
@@ -1,22 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '@/components/Login'
-import Home from '@/components/Home'
-import Truck from '@/components/truck/Truck'
-import TruckTeam from '@/components/truck/TruckTeam'
-import BindTruck from '@/components/truck/BindTruck'
-import Driver from '@/components/driver/Driver'
-import Capacity from '@/components/capacity/Capacity'
-import CapacityHistory from '@/components/capacity/CapacityHistory'
-import CostAccounting from '@/components/cost/CostAccounting'
-import CostMaintenance from '@/components/cost/CostMaintenance'
-import Schedule from '@/components/schedule/Schedule'
-import User from '@/components/system/User'
-import LogDic from '@/components/system/LogDic'
-import SysLog from '@/components/system/SysLog'
-import BillCreate from '@/components/waybill/BillCreate'
-import BillReceive from '@/components/waybill/BillReceive'
-import LoginLog from '@/components/system/LoginLog'
+// import Login from '@/components/Login'
+// import Home from '@/components/Home'
+// import Truck from '@/components/truck/Truck'
+// import TruckTeam from '@/components/truck/TruckTeam'
+// import BindTruck from '@/components/truck/BindTruck'
+// import Driver from '@/components/driver/Driver'
+// import Capacity from '@/components/capacity/Capacity'
+// import CapacityHistory from '@/components/capacity/CapacityHistory'
+// import CostAccounting from '@/components/cost/CostAccounting'
+// import CostMaintenance from '@/components/cost/CostMaintenance'
+// import Schedule from '@/components/schedule/Schedule'
+// import User from '@/components/system/User'
+// import LogDic from '@/components/system/LogDic'
+// import SysLog from '@/components/system/SysLog'
+// import BillCreate from '@/components/waybill/BillCreate'
+// import BillReceive from '@/components/waybill/BillReceive'
+// import LoginLog from '@/components/system/LoginLog'
 
 
 Vue.use(Router);
@@ -25,20 +25,20 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Login',
+      name: '登陆',
       hidden:true,
-      component: Login
+      component: ()=>import('@/components/Login'),
     },
     {
       path:'/home',
-      name:'Home',
+      name:'首页',
       hidden:true,
-      component:Home
+      component: ()=>import('@/components/Home'),
     },
     {
       path:'/home',
       name:'车辆管理',
-      component:Home,
+      component: ()=>import('@/components/Home'),
       icon:'el-icon-truck',
       meta:
         {
@@ -49,24 +49,24 @@ export default new Router({
         {
           path:'/truck',
           name:'车辆信息维护',
-          component:Truck
+          component: ()=>import('@/components/truck/Truck'),
         },
         {
           path:'/bindTruck',
           name:'绑定车辆信息',
-          component:BindTruck
+          component: ()=>import('@/components/truck/BindTruck'),
         },
         {
           path:'/truckTeam',
           name:'车队信息维护',
-          component:TruckTeam
+          component: ()=>import('@/components/truck/TruckTeam'),
         }
       ]
     },
     {
       path:'/home',
       name:'驾驶员管理',
-      component:Home,
+      component: ()=>import('@/components/Home'),
       icon:'el-icon-user',
       meta:
         {
@@ -77,13 +77,13 @@ export default new Router({
         {
           path:'/driver',
           name:'驾驶员信息维护',
-          component:Driver
+          component: ()=>import('@/components/driver/Driver'),
         }
       ]
     }, {
       path:'/home',
       name:'运力查询',
-      component:Home,
+      component: ()=>import('@/components/Home'),
       icon: 'el-icon-wind-power',
       meta:
         {
@@ -94,17 +94,17 @@ export default new Router({
         {
           path:'/capacity',
           name:'运力综合查询',
-          component:Capacity
+          component: ()=>import('@/components/capacity/Capacity'),
         },{
           path:'/capacityHistory',
           name:'历史承运任务查询',
-          component:CapacityHistory
+          component: ()=>import('@/components/capacity/CapacityHistory'),
         }
       ]
     }, {
       path:'/home',
       name:'运输成本核算',
-      component:Home,
+      component: ()=>import('@/components/Home'),
       meta:
         {
           rolesId:[3,9],
@@ -115,18 +115,18 @@ export default new Router({
         {
           path:'/costMaintenance',
           name:'车队运输成本维护',
-          component:CostMaintenance
+          component: ()=>import('@/components/cost/CostMaintenance'),
         },
         {
           path:'/costAccounting',
           name:'车队运输成本核算',
-          component:CostAccounting
+          component: ()=>import('@/components/cost/CostAccounting'),
         }
       ]
     }, {
       path:'/home',
       name:'调度任务',
-      component:Home,
+      component: ()=>import('@/components/Home'),
       meta:
         {
           rolesId:[5,9],
@@ -137,13 +137,13 @@ export default new Router({
         {
           path:'/schedule',
           name:'调度承运任务',
-          component:Schedule
+          component: ()=>import('@/components/schedule/Schedule'),
         }
       ]
     }, {
       path:'/home',
       name:'承运任务管理',
-      component:Home,
+      component:()=>import('@/components/Home'),
       meta:
         {
           rolesId:[4,9],
@@ -154,18 +154,18 @@ export default new Router({
         {
           path:'/billCreate',
           name:'承运单开出',
-          component:BillCreate
+          component: ()=>import('@/components/waybill/BillCreate'),
         },
         {
           path:'/billReceive',
           name:'承运单接收',
-          component:BillReceive
+          component: ()=>import('@/components/waybill/BillReceive'),
         }
       ]
     }, {
       path:'/home',
       name:'系统维护',
-      component:Home,
+      component:()=>import('@/components/Home'),
       icon:'el-icon-s-custom',
       meta:
         {
@@ -176,22 +176,22 @@ export default new Router({
         {
           path:'/user',
           name:'用户维护',
-          component:User
+          component: ()=>import('@/components/system/User'),
         },
         {
           path:'/logDic',
           name:'日志字典',
-          component:LogDic
+          component: ()=>import('@/components/system/LogDic'),
         },
         {
           path:'/sysLog',
           name:'系统日志',
-          component:SysLog
+          component: ()=>import('@/components/system/SysLog'),
         },
         {
           path:'/loginLog',
           name:'登陆日志',
-          component:LoginLog
+          component: ()=>import('@/components/system/LoginLog'),
         }
       ]
     },
